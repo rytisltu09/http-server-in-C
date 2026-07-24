a175b82bbd240456857a4d5d0d43570e72298f29
Rytis <rytis.danilevicius79@gmail.com>
2026-07-24 22:50:47 +0300

    1.1

--- http.c
+++ http.c
@@ -9,19 +9,25 @@
 
 struct sockaddr_in server_addr = {0};
 
+void handle_client(int server_fd);
+void parse_received(char *buffer, char *method, char *path);
+void send_response(int client_fd, char *status, char *body);
+
 int main(){
+    // Initialize server socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1){
         perror("socket");
         return EXIT_FAILURE;
     }
     printf("Created a socket!\n");
-
+    // Define port, family, address
     server_addr.sin_port = htons(PORT);
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     printf("Binding the socket\n");
+    // Bind the socket
     if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
         perror("binding");
         return EXIT_FAILURE;
@@ -34,30 +40,93 @@ int main(){
     }
 
     while (1){
-        int client_fd = accept(server_fd, NULL, NULL);
-        if (client_fd == -1){
-            perror("accept");
-            continue;
-        }
-        printf("Client connected!\n");
-        char buffer[1024];
-        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received <= 0){
-            perror("recv");
-        }
-        else{
-            buffer[bytes_received] = '\0';
-            printf("%s\n", buffer);
-        }
-        char response[] =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        "\r\n"
-        "Hello from the server!\n";
-        if (send(client_fd, response, strlen(response), 0) == -1){
-            perror("send");
-        }
-        close(client_fd);
+        handle_client(server_fd);
     }
     close(server_fd);
 }
+
+void handle_client(int server_fd)
+{
+    int client_fd = accept(server_fd, NULL, NULL);
+
+    if (client_fd == -1){
+        perror("accept");
+        return;
+    }
+
+    char buffer[1024];
+    int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+    if (bytes_received <= 0){
+        perror("recv");
+        close(client_fd);
+        return;
+    }
+
+    buffer[bytes_received] = '\0';
+
+    char method[32];
+    char path[256];
+
+    parse_received(buffer, method, path);
+
+    printf("%s %s\n", method, path);
+
+    if (strcmp(path, "/") == 0 || strcmp(path, "/home") == 0){
+        FILE *file = fopen("public/index.html", "r");
+        if (file){
+            char body[1024];
+            size_t n = fread(body, 1, sizeof(body) - 1, file);
+            body[n] = '\0';
+            send_response(client_fd, "200 OK", body);
+            fclose(file);
+        } else {
+            send_response(client_fd, "500 Internal Server Error", "Could not open index.html\n");
+        }
+    }
+    else if (strcmp(path, "/about") == 0){
+        FILE *file = fopen("public/about.html", "r");
+        if (file){
+            char body[1024];
+            size_t n = fread(body, 1, sizeof(body) - 1, file);
+            body[n] = '\0';
+            send_response(client_fd, "200 OK", body);
+            fclose(file);
+        } else {
+            send_response(client_fd, "500 Internal Server Error", "Could not open about.html\n");
+        }
+    }
+    else{
+        send_response(client_fd, "404 Not Found", "404\n");
+    }
+
+    close(client_fd);
+}
+
+void parse_received(char *buffer, char *method, char *path)
+{
+    if (sscanf(buffer, "%31s %255s", method, path) != 2){
+        strcpy(method, "");
+        strcpy(path, "");
+    }
+}
+void send_response(int client_fd, char *status, char *body)
+{
+    char response[1024];
+
+    snprintf(
+        response,
+        sizeof(response),
+        "HTTP/1.1 %s\r\n"
+        "Content-Type: text/html\r\n"
+        "\r\n"
+        "%s",
+        status,
+        body
+    );
+
+    if (send(client_fd, response, strlen(response), 0) == -1){
+        perror("send");
+    }
+}
+
